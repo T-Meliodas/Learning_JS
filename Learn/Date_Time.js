ee052e58b02316ce52c08002fbc24eaf347f9b85
thanh <tthhaannhh07@gmail.com>
2025-03-15 12:13:09 +0700

    update about Date and Time

--- Learn/Date_Time.js
+++ Learn/Date_Time.js
@@ -1,14 +1,18 @@
 let Date_Timecurrent = () => {
-  let date = new Date(2024, 2, 15);
+  let date = new Date(2024, 2, 6);
   console.log(date);
-  console.log(date.toLocaleDateString());
+  console.log(date.toLocaleDateString("vi-VN")); //Output: 06/03/2024(day/mmonth/year)
 };
-Date_Timecurrent();
-//Output: 2024-3-15
-//Output: 3/15/2024
 let Date_Time = () => {
+    let date = new Date();
+    console.log(date.toLocaleDateString("vi-VN")); // 15/03/2025
+    console.log(date.toLocaleTimeString("vi-VN")); // 10:30:00
+  };
+  
+  Date_Time();
+  
   let date = new Date();
-  console.log(date);
-};
-Date_Time();
-//Output: 2021-3-15T08:00:00.000Z
+  console.log(
+    `Day: ${date.getDate()} month: ${date.getMonth() + 1} year: ${date.getFullYear()}`
+  );
+  
